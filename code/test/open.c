031aaee53da4e511d04a0d31286076ab82fc2437
NhatUS03 <21120516@student.hcmus.edu.vn>
2023-11-07 16:02:32 +0700

    Complete Open ,Close,Remove

--- code/test/open.c
+++ code/test/open.c
@@ -11,8 +11,8 @@ int main()
     int fileID = Open("text.txt", 1);
     int fileID2 = Open("text2.txt", 0);
     int fileID3 = Open("text3.txt", 1);
-//    int rs_rm=Remove("text3.txt");
-//    int rs_rm2=Remove("text2.txt");
+    int rs_rm=Remove("");
+    int rs_rm2=Remove("text2.txt");
     Halt();
     return 0;
 }
